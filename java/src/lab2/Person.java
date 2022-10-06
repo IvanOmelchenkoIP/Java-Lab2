fed6d032e057ff6fcf7a6ab4d6542fb902d8b54d
IvanOmelchenkoIP <io25112002@gmail.com>
2022-10-06 15:24:00 +0300

    Add Person class
    Add Person class with some fields and needed methods for further
    impementation

---
+++ java/src/lab2/Person.java
@@ -0,0 +1,19 @@
+package lab2;
+
+public class Person {
+	private String firstname;
+	private String lastname;
+	private String phonenumber;
+	
+	Person(String fname, String lname, String pnumber) {
+		firstname = fname;
+		lastname = lname;
+		phonenumber = pnumber;
+	}
+	
+	public void equals() {}
+	
+	public void serialize() {}
+	
+	public void deserialize() {}
+}
